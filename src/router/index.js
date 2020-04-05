4d24e15d7d478c852e274434b0620988d6951983
Jatin Hariani <jtnh321@gmail.com>
2020-04-06 04:09:06 +0530

    move verify to root

--- src/router/index.js
+++ src/router/index.js
@@ -49,7 +49,7 @@ const routes = [
                 component: VerifyOTP
             },
             {
-                path: '/verify',
+                path: '/',
                 name: PassVerifier,
                 component: PassVerifier
             },
